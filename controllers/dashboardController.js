4c2ae444dfdab7c2fa229fd443311038e36f7691
Suresh-Makhija <72131299+Suresh-Makhija@users.noreply.github.com>
2021-04-30 15:53:49 +0530

    dashboard
    changes as discussed in dashboard

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -16,40 +16,17 @@ const pieChart = async (req, res, next) => {
 const patientHistoryData = async (req, res, next) => {
 try {
  let chartType =  req.query.chartType;
- if(chartType == "year"){
-User.aggregate(
-[
-    {
-        $group:
-        {
-            _id:
-            {
-                year: { $year: "$date" }
-            },
-            count: { $sum:1 }
-           ,
-             date: { $first: "$date" }
-        }
-    },
-    {
-        $project:
-        {
-            date:
-            {
-                $dateToString: { format: "%Y", date: "$date" }
-            },
-            count: 1,
-            _id: 0
-        }
-    }
-],function(err,result){res.status(200).json(result);});
 
-}
 
-if(chartType == "month")
+if(chartType == "year")
 {
+  let Chartyear =  req.query.year;
+  let lessYear = (parseInt(Chartyear)+1)+"-01-01";
+  let greatYear = Chartyear+"-01-01";
+
   User.aggregate(
   [
+    { $match: { date: { $lt: new Date(lessYear), $gte: new Date(greatYear) } } },
       {
           $group:
           {
@@ -76,16 +53,32 @@ if(chartType == "month")
   ],function(err,result){res.status(200).json(result);});
 }
 
-if(chartType == "week")
+if(chartType == "month")
 {
+  let currentMonth = 0;
+  let nextMonth = 0;
+if(req.query.month.length == 1)
+{
+  console.log(parseInt("0"+req.query.month));
+  nextMonth = parseInt("0"+req.query.month)  +2;
+  currentMonth = parseInt("0"+req.query.month)  +1;
+}
+else {
+  nextMonth = parseInt(req.query.month)  +2;
+  currentMonth = parseInt(req.query.month)  +1;
+}
+  let lessYear = req.query.year+"-"+nextMonth+"-01";
+  let greatYear = req.query.year+"-"+currentMonth+"-01";;
+console.log(greatYear);
   User.aggregate(
   [
+    { $match: { date: { $lt: new Date(lessYear), $gte: new Date(greatYear) } } },
       {
           $group:
           {
               _id:
               {
-                    day: { $dayOfMonth: "$date" }
+                  day: { $dayOfMonth: "$date" }
               },
               count: { $sum:1 }
              ,
@@ -97,15 +90,14 @@ if(chartType == "week")
           {
               date:
               {
-                  $dateToString: { format: "%d-%m-%Y", date: "$date" }
+                  $dateToString: { format: "%d", date: "$date" }
               },
               count: 1,
               _id: 0
           }
       }
-  ],function(err,result){res.status(200).json(result);});
+  ],function(err,result){console.log(result);res.status(200).json(result);});
 }
-
 } catch (error) {
         res.status(400).send(error.message);
     }
